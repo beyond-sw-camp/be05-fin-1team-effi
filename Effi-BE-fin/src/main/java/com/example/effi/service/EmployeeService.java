87bbf787993796e59aa04ff387c5dce1a3a04c3f
chankyuu <shin4438@gmail.com>
2024-05-17 17:22:23 +0900

    Feat: 로그인 기능 추가 controller service repository 를 통한 로그인 로직 구현, token provider 추가를 통한 로그인 성공 시 토큰 발급

--- Effi-BE-fin/src/main/java/com/example/effi/service/EmployeeService.java
+++ Effi-BE-fin/src/main/java/com/example/effi/service/EmployeeService.java
@@ -4,6 +4,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.effi.config.TokenProvider;
 import com.example.effi.domain.Employee;
 import com.example.effi.domain.SignInRequest;
 import com.example.effi.domain.SignInResponse;
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
 
     @Transactional(readOnly = true)
     public SignInResponse signIn(SignInRequest request) {
@@ -30,11 +32,13 @@ public class EmployeeService {
                     .msg("비밀번호가 일치하지 않습니다.")
                     .build();
         }
+        String token = tokenProvider.createToken(String.format("%s:%s", employee.getEmpNo(), employee.getRank()));
         return SignInResponse.builder()
                 .empNo(employee.getEmpNo())
                 .name(employee.getName())
                 .rank(employee.getRank())
                 .msg("로그인 성공")
+                .token(token)
                 .build();
     }
 }
