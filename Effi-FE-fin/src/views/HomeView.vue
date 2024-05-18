66cb8c5ef473e733fd7961c2d6ffa6b0283dda9d
KimHyeyeonn <85014114+KimHyeyeonn@users.noreply.github.com>
2024-05-18 19:44:54 +0900

    Feat: "Add login view" 로그인 화면 구현

--- Effi-FE-fin/src/views/HomeView.vue
+++ Effi-FE-fin/src/views/HomeView.vue
@@ -1,9 +1,28 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="container">
+    <Navigation class="navigation"/>
+    <!-- 여기에 다른 컨텐츠 추가 -->
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/LeftSidebar.vue'
+export default {
+  components: {
+    Navigation
+  }
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+.container {
+  display: flex;
+}
+
+.navigation {
+  width: 200px; /* 내비게이션의 너비 설정 */
+  height: 100vh; /* 전체 높이 */
+  position: fixed; /* 고정 위치 */
+  left: 0; /* 왼쪽에 위치 */
+}
+</style>
