c14f09c53882da6d38e214757a49d4b941457341
chankyuu <shin4438@gmail.com>
2024-05-21 11:22:11 +0900

    Feat : Token Filter 추가 기존에 발급만 되던 토큰을 사용하기 위한 필터와 필터처리에 필요한 기능 추가

--- Effi-BE-fin/src/main/java/com/example/effi/config/SecurityConfig.java
+++ Effi-BE-fin/src/main/java/com/example/effi/config/SecurityConfig.java
@@ -2,21 +2,31 @@ package com.example.effi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import lombok.RequiredArgsConstructor;
+
+@EnableMethodSecurity
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     private final String[] allowedUrls = {"/", "/api/auth/**"};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(CsrfConfigurer<HttpSecurity>::disable)
+                .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
                 .authorizeHttpRequests(requests ->
                         requests.requestMatchers(allowedUrls).permitAll()  // requestMatchers의 인자로 전달된 url은 모두에게 허용
                                 .anyRequest().authenticated()  // 그 외의 모든 요청은 인증 필요
@@ -24,6 +34,7 @@ public class SecurityConfig {
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )  // 세션을 사용하지 않으므로 STATELESS 설정
+                .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
                 .build();
     }
 
