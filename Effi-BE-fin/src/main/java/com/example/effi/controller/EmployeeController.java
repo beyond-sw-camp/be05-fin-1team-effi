e870ee160f13428212ac14e5889d3c893a6d6a89
chankyuu <shin4438@gmail.com>
2024-05-28 20:26:27 +0900

    Feat : SignOut 로그아웃 구현. 단 access 토큰은 로컬에서 삭제, refresh token 만 서버단에서 비활성화 시키는 것으로구현

--- Effi-BE-fin/src/main/java/com/example/effi/controller/EmployeeController.java
+++ Effi-BE-fin/src/main/java/com/example/effi/controller/EmployeeController.java
@@ -1,17 +1,20 @@
 package com.example.effi.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.effi.domain.DTO.GlobalResponse;
 import com.example.effi.domain.DTO.SignInRequest;
 import com.example.effi.domain.DTO.SignInResponse;
-import com.example.effi.domain.Entitiy.Employee;
+import com.example.effi.domain.DTO.SignOutRequest;
 import com.example.effi.service.EmployeeService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import lombok.RequiredArgsConstructor;
 
-
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,12 +23,35 @@ public class EmployeeController {
     private final EmployeeService employeeService;
 
     @PostMapping("/signin")
-    public ResponseEntity<SignInResponse> signIn(@RequestBody SignInRequest request) {
+    public ResponseEntity<GlobalResponse> signIn(@RequestBody SignInRequest request) {
         SignInResponse response = employeeService.signIn(request);
         if(response.getMsg().equals("로그인 성공")) {
-            return ResponseEntity.ok(response);
+            return ResponseEntity.ok().body(GlobalResponse.builder()
+                                            .message(response.getMsg())
+                                            .status(HttpStatus.OK.value())
+                                            .data(response)
+                                            .build());
         }
-        return ResponseEntity.badRequest().body(response);
+        return ResponseEntity.badRequest().body(GlobalResponse.builder()
+                                            .message(response.getMsg())
+                                            .status(HttpStatus.BAD_REQUEST.value())
+                                            .build());
+    }
+
+    @PostMapping("/signout")// 로그아웃. 단 request 에 empNo 를 직접 담아서 보내는 것이 아닌 토큰을 통해 empNo 를 가져오는 것으로 수정한다.
+    public ResponseEntity<GlobalResponse> signOut(@RequestBody Map<String, String> request)  throws JsonProcessingException {
+        String token = request.get("token");
+        String response = employeeService.signOut(token);
+        if(response.equals("로그아웃 실패")) {
+            return ResponseEntity.badRequest().body(GlobalResponse.builder()
+                                                        .message(response)
+                                                        .status(HttpStatus.BAD_REQUEST.value())
+                                                        .build());
+        }
+        return ResponseEntity.ok().body(GlobalResponse.builder()
+                                        .message(response)
+                                        .status(HttpStatus.OK.value())
+                                        .build());
     }
 
     @PreAuthorize("hasAuthority('사원')")
