8bd7d44c371713d473fa9fd2b76564625f3a9618
chankyuu <shin4438@gmail.com>
2024-06-10 11:10:04 +0900

    Fix : 데이터 꼬임을 풀기 위한 임시 커밋

---
+++ Effi-FE-fin/src/views/ModalTestView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <TagModal />
+    </div>
+</template>
+  
+<script>
+  import TagModal from './components/TagModal.vue';
+  
+  export default {
+    components: {
+      TagModal
+    }
+  };
+</script>
+  
