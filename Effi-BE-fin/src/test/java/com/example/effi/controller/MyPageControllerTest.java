4f3451fcfff42f27714c4551253113263ec456ac
hyeonjin-dot <guswls_2610@naver.com>
2024-06-19 10:05:26 +0900

    ?

--- Effi-BE-fin/src/test/java/com/example/effi/controller/MyPageControllerTest.java
+++ Effi-BE-fin/src/test/java/com/example/effi/controller/MyPageControllerTest.java
@@ -93,6 +93,7 @@ class MyPageControllerTest {
     }
 
     @Test
+    @DisplayName("타임존 업데이트 - 실패:잘못된 타임존")
     void updateTimezone_ShouldReturnBadRequest_WhenInvalidInput() throws Exception {
         doThrow(new IllegalArgumentException("Invalid input")).when(myPageService).updateEmployeeTimezone(any(MyPageUpdateDTO.class));
 
